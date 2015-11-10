0fe5039541011da9d44ccdfcc8717558c696dba9
Peter East <08peast@gmail.com>
2015-11-10 15:02:54 +0000

    Little fix

--- LoginScreenGui.py
+++ LoginScreenGui.py
@@ -4,7 +4,7 @@
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
-class LoginWindow:
+class LoginWindow(QMainWindow):    
     def __init__(self):
         super().__init__()
         print("[INFO] Created Login window")
