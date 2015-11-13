72b1f45cbad0e01d26cf39672ddf22355cc68a15
Peter East <08peast@gmail.com>
2015-11-13 10:05:36 +0000

    MAKING IT WoRK

--- LoginScreenGui.py
+++ LoginScreenGui.py
@@ -5,12 +5,27 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
 class LoginWindow(QMainWindow):
+
+
     def __init__(self):
         super().__init__()
         print("[INFO] Created Login window")
 
         self.main_title = QLabel("Welcome.")
+
+        # Create a labeled username feild:
+
+        self.username_item = QWidget()
+
+        self.username_layout = QHBoxLayout()
+        self.username_label = QLabel("Username:")
         self.username_input = QLineEdit()
+
+        self.username_layout.addWidget(self.username_label)
+
+        self.username_item.setLayout(self.username_layout)
+
+
         self.password_input = QLineEdit()
         self.submit_button = QPushButton("Login")
         self.help_button = QPushButton("?")
@@ -24,7 +39,7 @@ class LoginWindow(QMainWindow):
         self.layout = QVBoxLayout()
 
         self.layout.addWidget(self.main_title)
-        self.layout.addWidget(self.username_input)
+        self.layout.addWidget(self.username_item)
         self.layout.addWidget(self.password_input)
         self.layout.addWidget(self.submit_button)
         self.layout.addWidget(self.help_button)
