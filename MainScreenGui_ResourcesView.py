2b0a0efbb0a8261bcf2713350063993a41c908cb
Peter East <08peast@gmail.com>
2015-11-30 11:10:16 +0000

    General update

--- MainScreenGui_ResourcesView.py
+++ MainScreenGui_ResourcesView.py
@@ -21,4 +21,28 @@ class ResourcesView(QWidget):
         self.left_pane_layout = QVBoxLayout()
 
 
+        self.placeholder = QLabel(" placeholder ")
+        self.placeholder.setFixedSize(400, 400)
+        self.left_pane_layout.addWidget(self.placeholder)
+        # This is where I'll define the tabular view.
+        # there also needs to be some code for getting sections of information
+        # from the database - otherwise it won't work.
+
+        self.left_pane_container.setLayout(self.left_pane_layout)
+        self.pane_container_layout.addWidget(self.left_pane_container)
+        self.right_pane_container = QWidget()
+        self.right_pane_layout = QVBoxLayout()
+
+        self.add_items_button = QPushButton("Add Resource")
+        self.right_pane_layout.addWidget(self.add_items_button)
+
+        self.view_urgent_requirements_button = QPushButton("View urgent requirements")
+        self.right_pane_layout.addWidget(self.view_urgent_requirements_button)
+
+        self.right_pane_container.setLayout(self.right_pane_layout)
+        self.pane_container_layout.addWidget(self.right_pane_container)
+
+        self.pane_container.setLayout(self.pane_container_layout)
+        self.main_layout.addWidget(self.pane_container)
+
         self.setLayout(self.main_layout)
